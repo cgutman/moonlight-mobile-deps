02c97bc2e0b3a93394e7e9ba2b843e922c601e0c
Cameron Gutman <aicommander@gmail.com>
2023-11-04 02:30:01 -0500

    Try to fix preprocessor error on AppVeyor VM

--- opus.sh
+++ opus.sh
@@ -17,6 +17,7 @@ mkdir ../build/opus_$3_$1
 ./configure --prefix=$PWD/../build/opus_$3_$1 \
             --disable-shared --enable-static --with-pic --disable-extra-programs --disable-doc \
             --host=$4 \
+            CPP="$(xcrun -f clang) -arch $1 --target=$1-apple-$3 -E" \
             CFLAGS="$CFLAGS -O3 -g -m$3-version-min=$OS_MIN -arch $1 --target=$1-apple-$3 -isysroot $(xcrun --sdk $2 --show-sdk-path)" \
             LDFLAGS="$LDFLAGS -m$3-version-min=$OS_MIN -arch $1"
 make -j$(sysctl -n hw.ncpu)
