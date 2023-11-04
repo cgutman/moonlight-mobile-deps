44c5e7a7d3c9b766067d2ec80653194218a18444
Cameron Gutman <aicommander@gmail.com>
2023-11-04 00:15:26 -0500

    Add FFmpeg build with AV1 parser

---
+++ FFmpeg.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+# $1 - Architecture
+# $2 - SDK name (iphoneos/appletvos)
+# $3 - OS name (ios/tvos)
+
+pushd FFmpeg
+
+export SDKROOT=$(xcrun --sdk $2 --show-sdk-path)
+export LIBRARY_PATH="$LIBRARY_PATH:$SDKROOT/usr/lib"
+
+# Build FFmpeg
+mkdir ../build/FFmpeg_$3_$1
+./configure --prefix=$PWD/../build/FFmpeg_$3_$1 \
+            --extra-cflags="-m$3-version-min=$OS_MIN -arch $1 --target=$1-apple-$3 -isysroot $(xcrun --sdk $2 --show-sdk-path)" \
+            --extra-ldflags="-m$3-version-min=$OS_MIN -arch $1" \
+            --arch=$1 \
+            --enable-cross-compile --disable-all --disable-x86asm --enable-avcodec --enable-avformat --enable-muxer=flv
+make -j$(sysctl -n hw.ncpu)
+make install
+
+# Clean up in-tree build
+git reset --hard
+git clean -f -d -x
+popd
--- archive.sh
+++ archive.sh
@@ -1,18 +1,51 @@
 set -e
 
 mkdir output
+
 mkdir output/SDL2
 mkdir output/SDL2/include
 mkdir output/SDL2/lib
 mkdir output/SDL2/lib/iOS
+mkdir output/SDL2/lib/iOS-Sim
 mkdir output/SDL2/lib/tvOS
+mkdir output/SDL2/lib/tvOS-Sim
 
 cp SDL/include/*.h output/SDL2/include
 
-lipo SDL/Xcode/SDL/build/Release-iphone*/libSDL2.a -create -o output/SDL2/lib/iOS/libSDL2.a
-lipo SDL/Xcode/SDL/build/Release-appletv*/libSDL2.a -create -o output/SDL2/lib/tvOS/libSDL2.a
+cp SDL/Xcode/SDL/build/Release-iphoneos/libSDL2.a output/SDL2/lib/iOS/
+cp SDL/Xcode/SDL/build/Release-iphonesimulator/libSDL2.a output/SDL2/lib/iOS-Sim/
 
-lipo -info output/SDL2/lib/iOS/libSDL2.a
-lipo -info output/SDL2/lib/tvOS/libSDL2.a
+cp SDL/Xcode/SDL/build/Release-appletvos/libSDL2.a output/SDL2/lib/tvOS/
+cp SDL/Xcode/SDL/build/Release-appletvsimulator/libSDL2.a output/SDL2/lib/tvOS-Sim/
+
+mkdir output/FFmpeg
+mkdir output/FFmpeg/lib
+mkdir output/FFmpeg/lib/iOS
+mkdir output/FFmpeg/lib/iOS-Sim
+mkdir output/FFmpeg/lib/tvOS
+mkdir output/FFmpeg/lib/tvOS-Sim
+
+cp -R build/FFmpeg_ios_arm64/include output/FFmpeg/include
+
+lipo build/FFmpeg_ios_*/lib/libavcodec.a -create -o output/FFmpeg/lib/iOS/libavcodec.a
+lipo build/FFmpeg_ios_*/lib/libavformat.a -create -o output/FFmpeg/lib/iOS/libavformat.a
+lipo build/FFmpeg_ios_*/lib/libavutil.a -create -o output/FFmpeg/lib/iOS/libavutil.a
+
+lipo build/FFmpeg_ios-simulator_*/lib/libavcodec.a -create -o output/FFmpeg/lib/iOS-Sim/libavcodec.a
+lipo build/FFmpeg_ios-simulator_*/lib/libavformat.a -create -o output/FFmpeg/lib/iOS-Sim/libavformat.a
+lipo build/FFmpeg_ios-simulator_*/lib/libavutil.a -create -o output/FFmpeg/lib/iOS-Sim/libavutil.a
+
+lipo build/FFmpeg_tvos_*/lib/libavcodec.a -create -o output/FFmpeg/lib/tvOS/libavcodec.a
+lipo build/FFmpeg_tvos_*/lib/libavformat.a -create -o output/FFmpeg/lib/tvOS/libavformat.a
+lipo build/FFmpeg_tvos_*/lib/libavutil.a -create -o output/FFmpeg/lib/tvOS/libavutil.a
+
+lipo build/FFmpeg_tvos-simulator_*/lib/libavcodec.a -create -o output/FFmpeg/lib/tvOS-Sim/libavcodec.a
+lipo build/FFmpeg_tvos-simulator_*/lib/libavformat.a -create -o output/FFmpeg/lib/tvOS-Sim/libavformat.a
+lipo build/FFmpeg_tvos-simulator_*/lib/libavutil.a -create -o output/FFmpeg/lib/tvOS-Sim/libavutil.a
+
+lipo -info output/FFmpeg/lib/iOS/*.a
+lipo -info output/FFmpeg/lib/iOS-Sim/*.a
+lipo -info output/FFmpeg/lib/tvOS/*.a
+lipo -info output/FFmpeg/lib/tvOS-Sim/*.a
 
 7z a moonlight-ios-deps.zip ./output/*
