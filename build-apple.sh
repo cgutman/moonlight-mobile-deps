9e187541aba4aa0e9a4d69d53e9ce60c7df8722a
Cameron Gutman <aicommander@gmail.com>
2025-03-02 16:19:07 -0600

    Fix host triple for Apple Silicon

--- build-apple.sh
+++ build-apple.sh
@@ -1,11 +1,11 @@
 set -e
 
-./opus-apple.sh arm64 iphoneos ios arm-apple-darwin
-./opus-apple.sh arm64 iphonesimulator ios-simulator arm-apple-darwin
-./opus-apple.sh x86_64 iphonesimulator ios-simulator x86_64-apple-darwin
-./opus-apple.sh arm64 appletvos tvos arm-apple-darwin
-./opus-apple.sh arm64 appletvsimulator tvos-simulator arm-apple-darwin
-./opus-apple.sh x86_64 appletvsimulator tvos-simulator x86_64-apple-darwin
+./opus-apple.sh arm64 iphoneos ios
+./opus-apple.sh arm64 iphonesimulator ios-simulator
+./opus-apple.sh x86_64 iphonesimulator ios-simulator
+./opus-apple.sh arm64 appletvos tvos
+./opus-apple.sh arm64 appletvsimulator tvos-simulator
+./opus-apple.sh x86_64 appletvsimulator tvos-simulator
 ./FFmpeg.sh arm64 iphoneos ios
 ./FFmpeg.sh arm64 iphonesimulator ios-simulator
 ./FFmpeg.sh x86_64 iphonesimulator ios-simulator
--- opus-apple.sh
+++ opus-apple.sh
@@ -4,7 +4,6 @@ set -e
 # $1 - Architecture
 # $2 - SDK name (iphoneos/appletvos)
 # $3 - OS name (ios/tvos)
-# $4 - Opus host type
 
 tar xzf opus-$OPUS_VERSION.tar.gz
 pushd opus-$OPUS_VERSION
@@ -16,7 +15,7 @@ export LIBRARY_PATH="$LIBRARY_PATH:$SDKROOT/usr/lib"
 mkdir ../build/opus_$3_$1
 ./configure --prefix=$PWD/../build/opus_$3_$1 \
             --disable-shared --enable-static --with-pic --disable-extra-programs --disable-doc \
-            --host=$4 \
+            --host=$1-apple-darwin \
             CPP="$(xcrun -f clang) -arch $1 --target=$1-apple-$3 -E" \
             CFLAGS="$CFLAGS -O3 -g -m$3-version-min=$OS_MIN -arch $1 --target=$1-apple-$3 -isysroot $(xcrun --sdk $2 --show-sdk-path)" \
             LDFLAGS="$LDFLAGS -m$3-version-min=$OS_MIN -arch $1"
